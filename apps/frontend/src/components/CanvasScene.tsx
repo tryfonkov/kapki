3885340bd5f0ab969763abbf1e4f82d50545b537
tryfonkov <trifonkov@gmail.com>
2025-07-07 17:13:44 +0200

    chore: add .prettierignore to exclude build artifacts and dependencies

--- apps/frontend/src/components/CanvasScene.tsx
+++ apps/frontend/src/components/CanvasScene.tsx
@@ -1,33 +1,25 @@
-"use client";
+'use client';
 
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment} from "@react-three/drei";
-import { ModelLoader } from "./ModelLoader";
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Environment } from '@react-three/drei';
+import { ModelLoader } from './ModelLoader';
 
 export default function CanvasScene() {
   return (
     <div className="flex-1 relative">
-      <Canvas
-        camera={{ position: [3, 3, 3], fov: 50 }}
-        shadows
-      >
+      <Canvas camera={{ position: [3, 3, 3], fov: 50 }} shadows>
         {/* Ambient light */}
         <ambientLight intensity={0.5} />
 
         {/* Directional light */}
         <ambientLight intensity={0.3} />
         <directionalLight position={[5, 5, 5]} intensity={0.8} />
-        <hemisphereLight color={"white"} groundColor={"#444"} intensity={0.5} />
+        <hemisphereLight color={'white'} groundColor={'#444'} intensity={0.5} />
 
-         <ModelLoader url="/models/DamagedHelmet.glb" scale={1} />
+        <ModelLoader url="/models/DamagedHelmet.glb" scale={1} />
 
         {/* Camera controls */}
-        <OrbitControls
-          enablePan
-          enableZoom
-          enableRotate
-          enableDamping={false}
-        />
+        <OrbitControls enablePan enableZoom enableRotate enableDamping={false} />
 
         <Environment preset="sunset" />
       </Canvas>
